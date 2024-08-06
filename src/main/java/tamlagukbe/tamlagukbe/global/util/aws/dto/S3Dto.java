48285b41c6731614b1919e5540916f646f70c967
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-08-06 12:27:32 +0900

    add: 다양한 글로벌 설정, 예외 관리 및 AWS S3 및 Redis 서비스 추가
    - application.properties 삭제
    - 글로벌 예외 처리 클래스 추가
    - Redis 관련 서비스 및 설정 클래스 추가
    - AWS S3 관련 서비스 및 설정 클래스 추가
    - JPA Auditing 활성화 설정 추가
    - BaseEntity 추가

---
+++ src/main/java/tamlagukbe/tamlagukbe/global/util/aws/dto/S3Dto.java
@@ -0,0 +1,17 @@
+package tamlagukbe.tamlagukbe.global.util.aws.dto;
+
+import lombok.Builder;
+
+
+@Builder
+public record S3Dto(
+        Long id,
+        String url,
+        String fileName,
+        Long size
+) {
+
+
+
+
+}
